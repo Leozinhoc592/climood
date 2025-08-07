2ab9d322d54af5cb971d705eddc828974310d305
Leo Camara <leozinhoc592@gmail.com>
2025-08-06 21:10:26 -0300

    Dia do mes dinamico

--- src/components/ListaRegistros.jsx
+++ src/components/ListaRegistros.jsx
@@ -6,13 +6,14 @@ const ListaRegistros = () => {
   const dataAtual = new Date();
   const mesAtual = dataAtual.getMonth();
   const anoAtual = dataAtual.getFullYear();
+  const ultimoDiaMes = new Date(anoAtual, mesAtual + 1, 0).getDate();
 
   return (
     <>
     <div className="BlocoMes">   
   <h1>listaderegistrosteste</h1>
   <div className="dias-container">
-    {Array.from({ length: 28 }).map((_, i) => (
+    {Array.from({ length: ultimoDiaMes }).map((_, i) => (
       <BlocoDia key={i}>{i + 1}</BlocoDia>
     ))}
   </div>
